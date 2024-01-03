bc014244e90642d50645bb4387bb9bcbb6685590
rumeethundal <rumeethundal03@gmail.com>
2024-01-02 17:11:14 -0800

    added name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        Scanner hello = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = hello.nextLine();
+        System.out.print("Hello " + name);
+
     }
 
 }
